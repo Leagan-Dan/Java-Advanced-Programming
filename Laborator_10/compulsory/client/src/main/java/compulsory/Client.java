0759c78693f1d25bc20b2b1ddd8b4138f68f10ff
Leagan-Dan <leaganndanut@gmail.com>
2022-05-09 15:00:43 +0300

    Added lab 10 - compulsory

--- Laborator_10/compulsory/client/src/main/java/compulsory/Client.java
+++ Laborator_10/compulsory/client/src/main/java/compulsory/Client.java
@@ -1,2 +1,44 @@
-package compulsory;public class Client {
+package compulsory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    private String serverAddress = "127.0.0.1";
+    private int PORT = 8100;
+    private Scanner scanner = new Scanner(System.in);
+    private boolean exit = false;
+
+    public Client() {
+        while (!exit) {
+            try (
+                    Socket socket = new Socket(serverAddress, PORT);
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+                String command = scanner.nextLine();
+
+                out.println(command);
+                if (command.equals("exit")) {
+                    exit = true;
+                    break;
+                }
+
+                String response = in.readLine();
+                System.out.println(response);
+
+
+            } catch (UnknownHostException e) {
+                System.err.println("No server listening... " + e);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- Laborator_10/compulsory/client/src/main/java/compulsory/Main.java
+++ Laborator_10/compulsory/client/src/main/java/compulsory/Main.java
@@ -1,2 +1,9 @@
-package compulsory;public class Main {
+package compulsory;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Client client=new Client();
+    }
 }
